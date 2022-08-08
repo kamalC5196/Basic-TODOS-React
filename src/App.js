6576e8fac93777e53451ca7f9fe75500165d641b
Kamal C <34775518+kamalC5196@users.noreply.github.com>
2022-08-08 23:37:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState, useCallback } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const handleTodo = useCallback((e) => {
+    e.preventDefault();
+    const task = e.target.elements.enterTodo.value;
+    const id = Date.now();
+    const done = false;
+    //setTodos((prev) => [...prev, { task, id }]);
+    setTodos((prev) => prev.concat({ task, id, done }));
+  }, []);
+
+  const deleteTodo = useCallback((todo) => {
+    //const updatedTodos = todos.filter((x) => x.id !== todo.id);
+    setTodos((prev) => prev.filter((x) => x.id !== todo.id));
+  }, []);
+
+  const handleDone = useCallback((todo) => {
+    //const updatedTodos = todos.filter((x) => x.id !== todo.id);
+    setTodos((prev) =>
+      prev.map((x) => (x.id === todo.id ? { ...x, done: !x.done } : x))
+    );
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>TODO LIST</h1>
+      <form onSubmit={(e) => handleTodo(e)}>
+        <input type="text" id="enterTodo" />
+        <button type="submit">add</button>
+      </form>
+      {todos.map((todo) => (
+        <li
+          key={todo?.id}
+          onDoubleClick={() => {
+            deleteTodo(todo);
+          }}
+          onClick={() => handleDone(todo)}
+          style={{ textDecoration: todo.done ? "line-through" : "none" }}
+        >
+          {todo?.task}
+        </li>
+      ))}
+    </div>
+  );
+}
